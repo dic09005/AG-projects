ef4b7ef3aa6ba3e082d2fbd78e0ad7007ab149c1
rob-dickson <dickson.rob88@gmail.com>
2023-04-10 15:08:53 -0600

    commit

--- Projects/parallaxScrolling/js/main.js
+++ Projects/parallaxScrolling/js/main.js
@@ -7,4 +7,18 @@ function myFunction(x) {
     x.classList.toggle("change");
   }
 
-<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+let mybutton = document.getElementById('myBtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.setAttribute("style", "display: block;")
+    } else {
+        mybutton.setAttribute("style", "display: none;")
+    }
+  }
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
