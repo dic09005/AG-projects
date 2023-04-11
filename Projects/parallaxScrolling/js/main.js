729a81ed964c6b3d5f83971df4322cb55f75c467
rob-dickson <dickson.rob88@gmail.com>
2023-04-11 14:11:13 -0600

    commit

--- Projects/parallaxScrolling/js/main.js
+++ Projects/parallaxScrolling/js/main.js
@@ -29,6 +29,7 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+// Get JSON data for div.car 
 const requestURL = 'json/cars.json';
 const cards = document.querySelector('.card');
 
@@ -36,46 +37,77 @@ const cards = document.querySelector('.card');
 const carMake = document.querySelector('#make');
 const carModel = document.querySelector('#model');
 const carYears = document.querySelector('#years');
+const carPic = document.querySelector('#carPic');
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-      console.table(jsonObject); //for testing
+      //console.table(jsonObject); //for testing
       const cars = jsonObject;  
       displayRandomCar(cars);
   });
 
+// Make a function that can select a random make and model from the JSON file
 function displayRandomCar(data) {
 
-    let makeList = [0,1];
-
-    const values = Object.values(makeList);
-
-    const randomValue = values[parseInt(Math.random() * values.length)];
-
-
-
     let makes = ["honda","nissan","toyota","mazda","mitsubishi","subaru"];
-
     const value = Object.values(makes);
-
     const Value = value[parseInt(Math.random() * value.length)];
-    
-    console.log(randomValue);
-    console.log(Value);
 
-    let call1 = "data.";
-    let call2 = Value + "[" + randomValue + "]";
-    let call3 = ".make";
+    if (Value == "honda") {
 
-    let fullCall = `${call1}${call2}${call3}`;
+      let makeList = [0,1,2,3,4];
+      let values = Object.values(makeList);
+      let randomValue = values[parseInt(Math.random() * values.length)];
 
-    console.log(fullCall);
+      carMake.innerHTML = `<strong>Make: ${data.honda[randomValue].make}</strong>`;
+      carModel.innerHTML = `<strong>Model: ${data.honda[randomValue].model}</strong>`;
+      carYears.innerHTML = `<strong>Years: ${data.honda[randomValue].years}</strong>`;
 
-    carMake.innerHTML = `<strong>Make: ${fullCall}</strong>`;
-    carModel.innerHTML = `<strong>Model: ${fullCall}</strong>`;
-    carYears.innerHTML = `<strong>Years: ${fullCall}</strong>`;
+    } else if (Value == "nissan") {
+
+      let makeList = [0,1,2];
+      let values = Object.values(makeList);
+      let randomValue = values[parseInt(Math.random() * values.length)];
+
+      carMake.innerHTML = `<strong>Make: ${data.nissan[randomValue].make}</strong>`;
+      carModel.innerHTML = `<strong>Model: ${data.nissan[randomValue].model}</strong>`;
+      carYears.innerHTML = `<strong>Years: ${data.nissan[randomValue].years}</strong>`;
+
+    } else if (Value == "toyota") {
+
+      let makeList = [0,1,2];
+      let values = Object.values(makeList);
+      let randomValue = values[parseInt(Math.random() * values.length)];
+
+      carMake.innerHTML = `<strong>Make: ${data.toyota[randomValue].make}</strong>`;
+      carModel.innerHTML = `<strong>Model: ${data.toyota[randomValue].model}</strong>`;
+      carYears.innerHTML = `<strong>Years: ${data.toyota[randomValue].years}</strong>`;
+      
+    } else if (Value == "mazda") {
+
+      let makeList = [0,1];
+      let values = Object.values(makeList);
+      let randomValue = values[parseInt(Math.random() * values.length)];
+       
+      carMake.innerHTML = `<strong>Make: ${data.mazda[randomValue].make}</strong>`;
+      carModel.innerHTML = `<strong>Model: ${data.mazda[randomValue].model}</strong>`;
+      carYears.innerHTML = `<strong>Years: ${data.mazda[randomValue].years}</strong>`;
+      
+    } else if (Value == "mitsubishi") {
+
+      carMake.innerHTML = `<strong>Make: ${data.mitsubishi[0].make}</strong>`;
+      carModel.innerHTML = `<strong>Model: ${data.mitsubishi[0].model}</strong>`;
+      carYears.innerHTML = `<strong>Years: ${data.mitsubishi[0].years}</strong>`;
+      
+    } else if (Value == "subaru") {
+
+      carMake.innerHTML = `<strong>Make: ${data.subaru[0].make}</strong>`;
+      carModel.innerHTML = `<strong>Model: ${data.subaru[0].model}</strong>`;
+      carYears.innerHTML = `<strong>Years: ${data.subaru[0].years}</strong>`;
+      
+    };
 
 }
