cbefcd96ca15a89f3084391b67833e326bb06498
rob-dickson <dickson.rob88@gmail.com>
2023-04-11 12:27:12 -0600

    Commit

--- Projects/parallaxScrolling/js/main.js
+++ Projects/parallaxScrolling/js/main.js
@@ -28,3 +28,48 @@ function scrollFunction() {
 function topFunction() {
   document.documentElement.scrollTop = 0;
 }
+
+const requestURL = 'json/cars.json';
+const cards = document.querySelector('.card');
+
+// Create elements to add to the document
+const carMake = document.querySelector('#make');
+const carModel = document.querySelector('#model');
+const carYears = document.querySelector('#years');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+      console.table(jsonObject); //for testing
+      const cars = jsonObject;  
+      displayRandomCar(cars);
+  });
+
+function displayRandomCar(data) {
+
+    let makeList = [0,1,2,3,4];
+
+    const values = Object.values(makeList);
+
+    const randomValue = values[parseInt(Math.random() * values.length)];
+
+
+
+    let makes = ["honda","nissan","toyota","mazda","mitsubishi","subaru"];
+
+    const value = Object.values(makes);
+
+    const Value = value[parseInt(Math.random() * value.length)];
+    
+    console.log(randomValue);
+    console.log(Value);
+
+
+
+    carMake.innerHTML = `<strong>Make: ${data.honda[randomValue].make}</strong>`;
+    carModel.innerHTML = `<strong>Model: ${data.honda[randomValue].model}</strong>`;
+    carYears.innerHTML = `<strong>Years: ${data.honda[randomValue].years}</strong>`;
+
+}
